1bca9c8c0fed2e0031f8e7acaf9fe14bbfef418a
Artur Araujo <artur.stat@gmail.com>
2024-02-27 00:45:18 +0000

    Update floor_to.R
    Do not import anything from base. If we import from base, package installation fails:
    
    * installing *source* package 'flexOR' ...
    ** using staged installation
    ** R
    ** byte-compile and prepare package for lazy loading
    Error in asNamespace(ns, base.OK = FALSE) :
      operation not allowed on base namespace
    Calls: <Anonymous> ... namespaceImportFrom -> importIntoEnv -> getNamespaceInfo -> asNamespace
    Execution halted
    ERROR: lazy loading failed for package 'flexOR'
    * removing 'D:/CRAN/flexOR.Rcheck/flexOR'

--- R/floor_to.R
+++ R/floor_to.R
@@ -13,10 +13,9 @@
 #' floor_to(7, 3) # Returns 6, as 6 is the largest multiple of 3 less than or equal to 7.
 #' floor_to(5, 2) # Returns 4, as 4 is the largest multiple of 2 less than or equal to 5.
 #' 
-#' @importFrom base floor
 #' @export
 #'
 floor_to <- function(x, to=1) {
-  floored <- floor(x / to) * to
-  return(floored)
+  floored <- floor(x / to) * to;
+  return(floored);
 }#floor_to
